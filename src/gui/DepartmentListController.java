bf4661c9a4250403c61af66b90c7cf895f23e996
Matheus Maniero <matheusmaniero@gmail.com>
2022-06-03 16:34:04 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+
+	private DepartmentService service;
 
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -31,11 +37,17 @@ public class DepartmentListController implements Initializable {
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
+	
+	private ObservableList<Department> obsList;
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 
+	}
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 
 	private void initializeNodes() {
@@ -46,5 +58,32 @@ public class DepartmentListController implements Initializable {
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 
 	}
+	
+	public void updateTableView() {
+		
+		if (this.service == null) {
+			throw new IllegalCallerException("DepartmentService has not instantiate.");
+		}		
+		
+		List<Department> list = this.service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
